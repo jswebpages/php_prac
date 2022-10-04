ecb2bd37f0c3b6c08fb173cea97d8bf741e27fb0
Jerome <49881302+jswebpages@users.noreply.github.com>
2022-10-03 21:28:24 -0400

    index.php file

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/body.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+
+<p>copyright © JSwebpages.com <?php echo date("Y"); ?></p>
